6279fe15062243d1339e4d2980efa40d76cd8c0c
Nauce <naucelg@hotmail.com>
2016-05-21 01:25:55 +0200

    Rehecho de nuevo todo...

---
+++ sdi3-7.EJB/ejbModule/com/sdi/business/impl/RemoteEjbServicesLocator.java
@@ -0,0 +1,79 @@
+package com.sdi.business.impl;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import com.sdi.business.ApplicationService;
+import com.sdi.business.SeatService;
+import com.sdi.business.ServicesFactory;
+import com.sdi.business.TripService;
+import com.sdi.business.UserService;
+
+public class RemoteEjbServicesLocator implements ServicesFactory {
+
+	private static final String APPLICATION_SERVICE_JNDI_KEY =
+			"sdi3-7/"
+			+ "sdi3-7.EJB/"
+			+ "EjbApplicationService!"
+			+ "com.sdi.business.impl.RemoteApplicationService";
+	
+	private static final String SEAT_SERVICE_JNDI_KEY =
+			"sdi3-7/"
+			+ "sdi3-7.EJB/"
+			+ "EjbSeatService!"
+			+ "com.sdi.business.impl.RemoteSeatService";
+	
+	private static final String TRIP_SERVICE_JNDI_KEY =
+			"sdi3-7/"
+			+ "sdi3-7.EJB/"
+			+ "EjbTripService!"
+			+ "com.sdi.business.impl.RemoteTripService";
+	
+	private static final String USER_SERVICE_JNDI_KEY =
+			"sdi3-7/"
+			+ "sdi3-7.EJB/"
+			+ "EjbUserService!"
+			+ "com.sdi.business.impl.RemoteUserService";
+
+	@Override
+	public ApplicationService getApplicationService() {
+		try {
+			Context ctx = new InitialContext();
+			return (ApplicationService) ctx.lookup(APPLICATION_SERVICE_JNDI_KEY);
+		} catch (NamingException e) {
+			throw new RuntimeException("JNDI problem", e);
+		}
+	}
+
+	@Override
+	public SeatService getSeatService() {
+		try {
+			Context ctx = new InitialContext();
+			return (SeatService) ctx.lookup(SEAT_SERVICE_JNDI_KEY);
+		} catch (NamingException e) {
+			throw new RuntimeException("JNDI problem", e);
+		}
+	}
+	
+	@Override
+	public TripService getTripService() {
+		try {
+			Context ctx = new InitialContext();
+			return (TripService) ctx.lookup(TRIP_SERVICE_JNDI_KEY);
+		} catch (NamingException e) {
+			throw new RuntimeException("JNDI problem", e);
+		}
+	}
+	
+	@Override
+	public UserService getUserService() {
+		try {
+			Context ctx = new InitialContext();
+			return (UserService) ctx.lookup(USER_SERVICE_JNDI_KEY);
+		} catch (NamingException e) {
+			throw new RuntimeException("JNDI problem", e);
+		}
+	}
+
+}
---
+++ sdi3-7.EJB/ejbModule/com/sdi/business/impl/classes/TripsUpdateStatusTask.java
@@ -0,0 +1,57 @@
+package com.sdi.business.impl.classes;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import com.sdi.infrastructure.Factories;
+import com.sdi.model.Trip;
+import com.sdi.model.TripStatus;
+import com.sdi.persistence.TripDao;
+import com.sdi.persistence.util.DateUtil;
+
+public class TripsUpdateStatusTask {
+	public static final int interval = 3000;
+	
+	private Timer timer = new Timer();
+	
+	class Task extends TimerTask {
+
+		@Override
+		public void run() {
+//			TripDao dao = Factories.persistence.newTripDao();
+//			List<Trip> trips = dao.findTravelsOpenAndClosed();
+//			Date today = DateUtil.today();
+//			
+//			boolean modified = false;
+//			for (Trip trip:trips) {
+//				if (!trip.getStatus().equals(TripStatus.CANCELLED)) {
+//					if (trip.getArrivalDate().before(today)
+//							|| today.getTime() >= trip.getArrivalDate().getTime()) {
+//						trip.setStatus(TripStatus.DONE);
+//						modified = true;
+//					}
+//					
+//					if (trip.getClosingDate().before(today)
+//							|| today.getTime() >= trip.getClosingDate().getTime()) {
+//						trip.setStatus(TripStatus.CLOSED);
+//						modified = true;
+//					}
+//				}
+//				
+//				if (modified)
+//					dao.update(trip);
+//				
+//				modified = false;
+//			}
+
+		}
+		
+	}
+	
+	public TripsUpdateStatusTask() {
+		timer.schedule(new Task(), 0, interval);
+	}
+	
+}
