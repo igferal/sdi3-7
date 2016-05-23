995cdc9826e69448724e9476ec03843474fe75f4
nacho1014 <nachoveiga1014@gmail.com>
2016-05-24 00:18:39 +0200

    quitado un warning XD

--- sdi3-7.Web/src/com/sdi/presentation/BeanApplication.java
+++ sdi3-7.Web/src/com/sdi/presentation/BeanApplication.java
@@ -3,7 +3,6 @@ package com.sdi.presentation;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 
-import com.sdi.business.impl.classes.TripsUpdateStatusTask;
 
 /**
  * Maneja el mantenimiento programado de la base de datos sobre los estados
