5ab44b9199fded854048af3cf723f48f21fe4f80
Nauce <naucelg@hotmail.com>
2016-05-24 02:55:23 +0200

    Ratings en bd y Timer comentado

--- sdi3-7.EJB/ejbModule/com/sdi/business/impl/classes/TripsUpdateStatusTask.java
+++ sdi3-7.EJB/ejbModule/com/sdi/business/impl/classes/TripsUpdateStatusTask.java
@@ -18,48 +18,48 @@ import com.sdi.model.TripStatus;
 import com.sdi.persistence.TripDao;
 import com.sdi.persistence.util.DateUtil;
 
-@Singleton
-@LocalBean
-@Startup
+//@Singleton
+//@LocalBean
+//@Startup
 public class TripsUpdateStatusTask {
 	public static final int interval = 3000;
 
-	@Resource
-	private TimerService timerService;
-
-	@PostConstruct
-	private void init() {
-		timerService.createTimer(interval, "TaskTrips");
-	}
-
-	@Timeout
-	public void execute(Timer timer) {
-		TripDao dao = Factories.persistence.newTripDao();
-		List<Trip> trips = dao.findTravelsOpenAndClosed();
-		Date today = DateUtil.today();
-
-		boolean modified = false;
-		for (Trip trip : trips) {
-			if (!trip.getStatus().equals(TripStatus.CANCELLED)) {
-				if (trip.getArrivalDate().before(today)
-						|| today.getTime() >= trip.getArrivalDate().getTime()) {
-					trip.setStatus(TripStatus.DONE);
-					modified = true;
-				}
-
-				if (trip.getClosingDate().before(today)
-						|| today.getTime() >= trip.getClosingDate().getTime()) {
-					trip.setStatus(TripStatus.CLOSED);
-					modified = true;
-				}
-			}
-
-			if (modified)
-				dao.update(trip);
-
-			modified = false;
-		}
-
-	}
+//	@Resource
+//	private TimerService timerService;
+//
+//	@PostConstruct
+//	private void init() {
+//		timerService.createTimer(0, interval, "TaskTrips");
+//	}
+//
+//	@Timeout
+//	public void execute(Timer timer) {
+//		TripDao dao = Factories.persistence.newTripDao();
+//		List<Trip> trips = dao.findTravelsOpenAndClosed();
+//		Date today = DateUtil.today();
+//
+//		boolean modified = false;
+//		for (Trip trip : trips) {
+//			if (!trip.getStatus().equals(TripStatus.CANCELLED)) {
+//				if (trip.getArrivalDate().before(today)
+//						|| today.getTime() >= trip.getArrivalDate().getTime()) {
+//					trip.setStatus(TripStatus.DONE);
+//					modified = true;
+//				}
+//
+//				if (trip.getClosingDate().before(today)
+//						|| today.getTime() >= trip.getClosingDate().getTime()) {
+//					trip.setStatus(TripStatus.CLOSED);
+//					modified = true;
+//				}
+//			}
+//
+//			if (modified)
+//				dao.update(trip);
+//
+//			modified = false;
+//		}
+//
+//	}
 
 }
